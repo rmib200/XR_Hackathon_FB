29ccad59b6c6683861731ff5ce9d99b88f60e0f3
Manuel Illanes <rmib200@gmail.com>
2021-11-21 10:28:26 -0400

    fucking fixes

--- Assets/_manuel/MyScripts/handsGUIMic.cs
+++ Assets/_manuel/MyScripts/handsGUIMic.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class handsGUIMic : MonoBehaviour
 {
+    public GameObject particleEffectUI;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,12 @@ public class handsGUIMic : MonoBehaviour
     public void OnTriggerEnter(Collider other) {
         if(other.gameObject.name == "index_ui")
         print("Hands GUI Mic is ON");
-        
+         var rotation = new Quaternion();
+        // rotation.eulerAngles = new Vector3(transform.localRotation.x+90f, transform.localRotation.y, transform.localRotation.z);
+        rotation.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z+90f);
+        // Instantiate(particleEffectUI, transform.position, transform.localRotation);
+        // Instantiate(particleEffectUI, transform.position, transform.rotation);
+        Quaternion newRotation = Quaternion.LookRotation(Vector3.up);
+        Instantiate(particleEffectUI, transform.position, Quaternion.identity);
     }
 }
